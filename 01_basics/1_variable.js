f250b2b2d905e7059b0e3da1169a61fd363f8c78
Praduman Kushwaha <110069651+Pard235689@users.noreply.github.com>
2024-02-10 23:21:19 +0000

    new

--- 01_basics/1_variable.js
+++ 01_basics/1_variable.js
@@ -1,1 +1,13 @@
-console.log('Praduman Kushwaha')
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://private-anon-a8c9047f63-mailtrap.apiary-mock.com/api/v1/companies');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
+
+request.send();
